62f55a34dbd7cb3c25657cfde71727a8bf562913
BananaH <m8a2s0o4n23@gmail.com>
2015-10-16 11:47:35 +0800

    Finish Q2 rec version
    Add a global variable 'level' for counting recursive stages. Everytime the
    code entering the function level++. Before return the value, level --.

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,28 +1,21 @@
-/* FIXME: Implement! */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
+
+#define size 20
 
 char smallest_character(char str[], char c){
 	int num=0;
-	while((int)str[num]!=127)
-		num++;
-	for(int i=0;i<num;i++){
+	for(int i=0;i<strlen(str);i++){
 		if(((int)c)<((int)str[i]))
 			return str[i];
 	}
 	return str[0];
 }
 
-int main()
-{
-	char a[5] = {'c','f','j','p','v'};
-	char b[3] = {'c','c','k'};
-	int ai =strlen(a);
-	int bi =strlen(b);
-	printf("%s \n", a);
-	printf("%s \n", b);
+int main(){
+	char a[size] = {'c','f','j','p','v'};
+	char b[size] = {'c','c','k'};
 	char ans=smallest_character(a,'a');
 	printf("%c\n", ans);
 	ans=smallest_character(a,'c');
@@ -31,5 +24,5 @@ int main()
 	printf("%c\n", ans);
 	ans=smallest_character(b,'f');
 	printf("%c\n", ans);
-    	return 0;
+	return 0;
 }
--- q2/recursive.c
+++ q2/recursive.c
@@ -1,19 +1,38 @@
+/* FIXME: Implement! */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define size 20
+int level = 0;
 
 char smallest_character(char str[], char c){
-	int num=0;
-	for(int i=0;i<strlen(str);i++){
-		if(((int)c)<((int)str[i]))
-			return str[i];
+	char ans='?';
+	level++;
+	if((int)(*str)!=0){
+		if((int)(*str)>(int)c){
+			level--;
+			return *str;
+		}
+		else
+			ans = smallest_character((str+1),c);
+		if(ans=='?'&&level==1){
+			level--;
+			return *str;
+		}
+		else{
+			level--;
+			return ans;
+		}
 	}
-	return str[0];
+	else{
+		level--;
+		return ans;
+	}
 }
 
-int main(){
+int main()
+{
 	char a[size] = {'c','f','j','p','v'};
 	char b[size] = {'c','c','k'};
 	char ans=smallest_character(a,'a');
@@ -24,5 +43,5 @@ int main(){
 	printf("%c\n", ans);
 	ans=smallest_character(b,'f');
 	printf("%c\n", ans);
-	return 0;
+    	return 0;
 }
